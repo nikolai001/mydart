824332c50e7a95ffb8d1b4e581f532e80f8116fd
Nikolai Raahauge <nikolai@raahauge.eu>
2024-12-13 22:03:43 +0100

    Tailwind + icons

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import './index.css'
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(mdiVue, { icons: mdijs })
+    .use(router)
+    .mount('#app')
