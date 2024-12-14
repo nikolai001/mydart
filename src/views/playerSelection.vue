5f3d6d0faf91e055c2306161bca468055fabb146
Nikolai Raahauge <nikolai@raahauge.eu>
2024-12-14 21:54:52 +0100

    Can remove players

--- src/views/playerSelection.vue
+++ src/views/playerSelection.vue
@@ -3,7 +3,10 @@ import PlayerModal from "@/components/PlayerModal.vue";
 import { Player } from "@/types/game";
 import { computed, ref } from "vue";
 
-const players = ref<Player[]>([]);
+const players = ref<Player[]>([
+  { id: 1, name: "bo", average: 0, points: 0 },
+  { id: 2, name: "preben", average: 0, points: 0 },
+]);
 const playerModalShown = ref(false);
 
 const noPlayers = computed(() => {
@@ -21,15 +24,39 @@ function addNewPlayer(player: string | Player) {
     });
   }
 }
+
+function removePlayer(player: Player) {
+  const index = players.value.findIndex(
+    (currentPlayer) => currentPlayer.id === player.id
+  );
+  if (index !== -1) {
+    players.value.splice(index, 1);
+  }
+}
 </script>
 
 <template>
   <main class="flex flex-col bg-gray-200 h-screen">
-    <section class="flex flex-col pt-2">
+    <section class="flex flex-col pt-2 justify-items-center w-10/12 mx-auto">
       <span v-if="noPlayers" v-text="'No players'" class="text-center" />
-      <div v-for="player in players" class="flex">
-        <span v-text="player.name" />
-        <span v-text="player.average ?? '-'" />
+      <div class="rounded-md overflow-hidden shadow-sm">
+        <div
+          v-for="player in players"
+          class="flex mx-auto w-full even:bg-gray-400 odd:bg-gray-500 text-white py-2 px-4"
+        >
+          <span :key="player.id" v-text="player.name" />
+          <span
+            :key="player.id"
+            v-text="player.average + ' avg'"
+            class="ml-auto mr-10"
+          />
+          <button
+            class="flex justify-center items-center"
+            @click="removePlayer(player)"
+          >
+            <mdicon name="close" class="text-red-500 hover:text-red-200" />
+          </button>
+        </div>
       </div>
       <div class="flex ml-auto mr-3 items-center space-x-2">
         <span v-text="'Add player(s)'" class="text-sky-500" />
